7d3e8e5cd7a9d0defb222514a875ad1a6e79f9e8
Adam Friedman <adam@blok.party>
2020-07-24 12:56:12 -0700

    Started on Particle class.  Updated constructor

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ canvas.height = window.innerHeight;
 let numberOfParticles = 200;
 const particlesArray = [];
 let hue = 0;
+
+class Particle {
+	constructor() {
+		this.x = Math.random() * canvas.width;
+		this.y = Math.random() * canvas.height;
+		this.radius = Math.random() * 10 + 2;
+		this.speedX = Math.random() * 3 - 1.5;
+		this.speedY = Math.random() * 3 - 1.5;
+	}
+}
